576929553dd65327a07cddf3f0f23d29d31967c9
Samuel S. Faw <fawss@1038.org>
2025-01-27 19:18:53 -0500

    Climb subsystem skeleton code

---
+++ src/main/java/frc/robot/constants/ClimbConstants.java
@@ -0,0 +1,7 @@
+package frc.robot.constants;
+
+public class ClimbConstants {
+    public static final int climbMotorPort = 0;
+    public static final double climbUpSpeed = 0.0;
+    public static final double climbDownSpeed = 0.0;
+}
