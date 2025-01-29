a828bb7c72c7cb099093abb3e3ed004f90916b19
Rachit Gupta <rachitgupta1901@gmail.com>
2025-01-29 18:16:49 -0500

    Working on Wrist Subsystem

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -1,5 +1,36 @@
 package frc.robot.subsystems;
 
-public class Wrist {
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.WristConstants;
+
+public class Wrist extends SubsystemBase {
+
+    private SparkFlex wrist;
+    private AbsoluteEncoder wristEncoder;
+    private PIDController wristController;
+
+    private Wrist instance;
+
+    private Wrist() {
+        wrist = new SparkFlex(WristConstants.kWristCanId, MotorType.kBrushless);
+        // wristEncoder = new wristEncoder();
+
+    }
+
+    public Wrist getInstance() {
+        if (instance == null) {
+            instance = new Wrist();
+        }
+        return instance;
+    }
+
+    public void useOutput(double output, double setpoint) {
+
+    }
 
 }
