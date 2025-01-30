525f1b7be16effd6165c52e80f7ad77a135cf996
Murat Bayraktar <bmuratb502@gmail.com>
2025-01-30 18:57:52 -0500

    Work on wrist.

--- src/main/java/frc/robot/constants/WristConstants.java
+++ src/main/java/frc/robot/constants/WristConstants.java
@@ -1,5 +1,29 @@
 package frc.robot.constants;
 
 public class WristConstants {
+    public enum WristSetPoints {
+
+        L1_CORAL(0),
+        L2_CORAL(0),
+        L3_CORAL(0),
+        L4_CORAL(0),
+        L23_CORAL(0),
+        L34_CORAL(0),
+        PROCESSOR(0),
+        GROUND_ALGAE(0),
+        STORAGE(0),
+        FEEDER_STATION(0);
+
+        private double setpoint;
+
+        private WristSetPoints(double setpoint) {
+            this.setpoint = setpoint;
+        }
+
+        public double getSetpoint() {
+            return this.setpoint;
+        }
+    }
+
     public static final int kWristCanId = 0;
 }
--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -2,27 +2,36 @@ package frc.robot.subsystems;
 
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.NeoMotorConstants;
 import frc.robot.constants.WristConstants;
 
 public class Wrist extends SubsystemBase {
 
-    private SparkFlex wrist;
-    private AbsoluteEncoder wristEncoder;
-    private PIDController wristController;
-
-    private Wrist instance;
+    private static SparkFlex wristMotor;
+    private static AbsoluteEncoder wristEncoder;
+    private static PIDController wristController;
+    private static Wrist instance;
 
     private Wrist() {
-        wrist = new SparkFlex(WristConstants.kWristCanId, MotorType.kBrushless);
-        // wristEncoder = new wristEncoder();
-
+        wristMotor = new SparkFlex(WristConstants.kWristCanId, MotorType.kBrushless);
+        SparkFlexConfig wristConfig = new SparkFlexConfig();
+        wristConfig
+                .idleMode(IdleMode.kBrake)
+                .smartCurrentLimit(NeoMotorConstants.kMaxVortexCurrent)
+                .inverted(false);
+        wristMotor.configure(wristConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        wristEncoder = wristMotor.getAbsoluteEncoder();
     }
 
-    public Wrist getInstance() {
+    public static Wrist getInstance() {
         if (instance == null) {
             instance = new Wrist();
         }
