b0678320a4a979cefa155c1fc71df125c49fa963
Murat Bayraktar <bmuratb502@gmail.com>
2025-01-30 15:55:21 -0500

    Make Acquisition.

---
+++ src/main/java/frc/robot/constants/AcquisitionConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.constants;
+
+public class AcquisitionConstants {
+    public static final int TOP_LASER_CHANNEL = 0;
+    public static final int BOTTOM_LASER_CHANNEL = 0;
+    public static final int MOTOR_DEVICE_ID = 0;
+    public static final double ACQUIRE_SPEED = 0;
+    public static final double DISPOSE_SPEED = 0;
+}
--- src/main/java/frc/robot/subsystems/Acquisition.java
+++ src/main/java/frc/robot/subsystems/Acquisition.java
@@ -1,7 +1,60 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLimitSwitch;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.AcquisitionConstants;
+import frc.robot.constants.NeoMotorConstants;
 
 public class Acquisition extends SubsystemBase {
+    private static DigitalInput topLaser = new DigitalInput(AcquisitionConstants.TOP_LASER_CHANNEL);
+    private static DigitalInput bottomLaser = new DigitalInput(AcquisitionConstants.BOTTOM_LASER_CHANNEL);
+    private static SparkFlex acquisitionMotor = new SparkFlex(AcquisitionConstants.MOTOR_DEVICE_ID,
+            MotorType.kBrushless);
+    private static SparkLimitSwitch algaeSwitch = acquisitionMotor.getReverseLimitSwitch();
+    public static Acquisition instance;
 
+    private Acquisition() {
+        SparkFlexConfig acquisitionConfig = new SparkFlexConfig();
+        acquisitionConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(NeoMotorConstants.kMaxNeoCurrent);
+        acquisitionMotor.configure(acquisitionConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+    public static Acquisition getInstance() {
+        if (instance == null) {
+            instance = new Acquisition();
+        }
+        return instance;
+    }
+
+    public void acquire() {
+        acquisitionMotor.set(AcquisitionConstants.ACQUIRE_SPEED);
+    }
+
+    public void dispose() {
+        acquisitionMotor.set(AcquisitionConstants.DISPOSE_SPEED);
+    }
+
+    public void stopAcquisition() {
+        acquisitionMotor.stopMotor();
+    }
+
+    public boolean getTopLaser() {
+        return topLaser.get();
+    }
+
+    public boolean getBottomLaser() {
+        return bottomLaser.get();
+    }
+
+    public boolean getAlgaeSwitch() {
+        return algaeSwitch.isPressed();
+    }
 }
