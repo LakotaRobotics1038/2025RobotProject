09dee24f112abf276c8351f7f39eae7883307a7d
Wesley Reed <reed.wesley.s@gmail.com>
2025-01-30 17:12:23 -0500

    fix license error

--- src/main/java/frc/robot/constants/SwerveConstants.java
+++ src/main/java/frc/robot/constants/SwerveConstants.java
@@ -62,8 +62,7 @@ public class SwerveConstants {
     private static final SteerMotorArrangement kSteerMotorType = SteerMotorArrangement.TalonFX_Integrated;
 
     // The remote sensor feedback type to use for the steer motors;
-    // When not Pro-licensed, Fused*/Sync* automatically fall back to Remote*
-    private static final SteerFeedbackType kSteerFeedbackType = SteerFeedbackType.FusedCANcoder;
+    private static final SteerFeedbackType kSteerFeedbackType = SteerFeedbackType.RemoteCANcoder;
 
     // The stator current at which the wheels start to slip;
     // This needs to be tuned to your individual robot
