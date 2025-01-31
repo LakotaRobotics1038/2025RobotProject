c4bdb7cf8e62118c5582abb8ab3001f41dce48ae
Murat Bayraktar <bmuratb502@gmail.com>
2025-01-30 19:00:53 -0500

    Update src/main/java/frc/robot/constants/AcquisitionConstants.java
    Co-authored-by: Wesley Reed <24629474+reediculous456@users.noreply.github.com>

--- src/main/java/frc/robot/constants/AcquisitionConstants.java
+++ src/main/java/frc/robot/constants/AcquisitionConstants.java
@@ -1,8 +1,8 @@
 package frc.robot.constants;
 
 public class AcquisitionConstants {
-    public static final int kTopLaserChannel = 0;
-    public static final int kBottomLaserChannel = 0;
+    public static final int kTopLaserPort = 0;
+    public static final int kBottomLaserPort = 0;
     public static final int kAquisitionMotorPort = 0;
     public static final double kAcquireSpeed = 0;
     public static final double kDisposeSpeed = 0;
