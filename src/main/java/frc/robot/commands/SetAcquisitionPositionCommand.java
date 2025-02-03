54926de8afe9a63c6aa6386ff904b50647d95ad5
Rachit Gupta <rachitgupta1901@gmail.com>
2025-02-03 17:01:28 -0500

    Starting on SetAcquisitonPositionCommand; seems to be an error with Arm.java

---
+++ src/main/java/frc/robot/commands/SetAcquisitionPositionCommand.java
@@ -0,0 +1,36 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.constants.ArmConstants.ArmSetpoint;
+import frc.robot.subsystems.Arm;
+
+public class SetAcquisitionPositionCommand extends Command {
+
+    // private Shoulder shoulder;
+    // private Wrist wrist;
+    private Arm arm;
+    private ArmSetpoint setpoint;
+
+    public SetAcquisitionPositionCommand(ArmSetpoint setpoint) {
+        this.arm = Arm.getInstance();
+        this.setpoint = setpoint;
+    }
+
+    public void initialize() {
+        arm.enable();
+
+    }
+
+    public void execute() {
+        arm.setSetpoint(setpoint);
+    }
+
+    public boolean isFinished() {
+        return false;
+    }
+
+    public void end() {
+
+    }
+
+}
