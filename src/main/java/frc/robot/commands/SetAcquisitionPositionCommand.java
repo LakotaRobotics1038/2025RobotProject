b404b5f76a0a75ecd5ea91cc210e200ea38a4715
Rachit Gupta <rachitgupta1901@gmail.com>
2025-02-04 16:44:50 -0500

    Working on SetAcquisitionPositionCommand; this is likely incorrect based on the UML

--- src/main/java/frc/robot/commands/SetAcquisitionPositionCommand.java
+++ src/main/java/frc/robot/commands/SetAcquisitionPositionCommand.java
@@ -2,34 +2,51 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.constants.ArmConstants.ArmSetpoint;
+import frc.robot.constants.ShoulderConstants.ShoulderSetpoints;
+import frc.robot.constants.WristConstants.WristSetPoints;
 import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Shoulder;
+import frc.robot.subsystems.Wrist;
 
 public class SetAcquisitionPositionCommand extends Command {
 
-    // private Shoulder shoulder;
-    // private Wrist wrist;
+    private Shoulder shoulder;
+    private Wrist wrist;
     private Arm arm;
-    private ArmSetpoint setpoint;
+    private ShoulderSetpoints shoulderSetpoint;
+    private WristSetPoints wristSetpoint;
+    private ArmSetpoint armSetpoint;
 
-    public SetAcquisitionPositionCommand(ArmSetpoint setpoint) {
+    public SetAcquisitionPositionCommand(ShoulderSetpoints shoulderSetpoint, WristSetPoints wristSetpoint,
+            ArmSetpoint armSetpoint) {
+        this.shoulder = Shoulder.getInstance();
+        this.wrist = Wrist.getInstance();
         this.arm = Arm.getInstance();
-        this.setpoint = setpoint;
+        this.shoulderSetpoint = shoulderSetpoint;
+        this.wristSetpoint = wristSetpoint;
+        this.armSetpoint = armSetpoint;
     }
 
     public void initialize() {
+        wrist.enable();
+        shoulder.enable();
         arm.enable();
-
+        shoulder.setSetpoint(shoulderSetpoint);
+        wrist.setSetpoint(wristSetpoint);
+        arm.setSetpoint(armSetpoint);
     }
 
     public void execute() {
-        arm.setSetpoint(setpoint);
+
     }
 
     public boolean isFinished() {
-        return false;
+        return (wrist.onTarget() && shoulder.onTarget() && arm.onTarget()) || arm.isPressed();
     }
 
     public void end(boolean isInterrupted) {
-
+        wrist.disable();
+        shoulder.disable();
+        arm.disable();
     }
 }
