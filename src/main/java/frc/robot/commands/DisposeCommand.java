f9fd14621ea92e7d473ae855e0bfd89afbc86abc
Maryam A. Tunkara <tunkarama@1038.org>
2025-02-03 19:08:59 -0500

    fixed dispose acuqistion logic

--- src/main/java/frc/robot/commands/DisposeCommand.java
+++ src/main/java/frc/robot/commands/DisposeCommand.java
@@ -1,23 +1,40 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Acquisition;
 
 public class DisposeCommand extends Command {
     private Acquisition acquisition = Acquisition.getInstance();
+    private double secondsToDispose = 0.0;
+    private Timer timer = new Timer();
 
     public DisposeCommand() {
         super.addRequirements(acquisition);
     }
 
+    public DisposeCommand(double secondsToDispose) {
+        this.secondsToDispose = secondsToDispose;
+
+    }
+
     @Override
     public void execute() {
+        timer.restart();
         acquisition.dispose();
     }
 
     @Override
     public boolean isFinished() {
-        return !(acquisition.getTopLaser() || acquisition.getBottomLaser() || acquisition.getAlgaeSwitch());
+        if (!(acquisition.getTopLaser() || acquisition.getBottomLaser())) {
+            if (!(acquisition.getAlgaeSwitch())) {
+                return secondsToDispose == 0.0 ? false : timer.get() >= secondsToDispose;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 
     @Override
