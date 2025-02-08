0ab95bdab152dd21a00ea3898cecee694df150da
eogegeag <you@example.com>
2025-02-08 16:19:08 -0500

    updated constant

--- src/main/java/frc/robot/constants/AcquisitionConstants.java
+++ src/main/java/frc/robot/constants/AcquisitionConstants.java
@@ -4,6 +4,6 @@ public class AcquisitionConstants {
     public static final int kTopLaserPort = 0;
     public static final int kBottomLaserPort = 10;
     public static final int kAcquisitionMotorPort = 9;
-    public static final double kAcquireSpeed = 0.75;
+    public static final double kAcquireSpeed = 1.0;
     public static final double kDisposeSpeed = -1.0;
 }
