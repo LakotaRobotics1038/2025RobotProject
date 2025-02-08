00ef6d38052fb57d95770d3b6901ee7808f3f4bb
Murat65536 <bob65536@proton.me>
2025-02-08 12:59:43 -0500

    testing

--- src/main/java/frc/robot/OperatorJoystick.java
+++ src/main/java/frc/robot/OperatorJoystick.java
@@ -1,6 +1,8 @@
 package frc.robot;
 
 import frc.robot.libraries.XboxController1038;
+import frc.robot.commands.AcquireCommand;
+import frc.robot.commands.DisposeCommand;
 import frc.robot.constants.IOConstants;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -20,5 +22,7 @@ public class OperatorJoystick extends XboxController1038 {
 
     private OperatorJoystick() {
         super(IOConstants.kOperatorControllerPort);
+        aButton.whileTrue(new AcquireCommand());
+        bButton.whileTrue(new DisposeCommand(3));
     }
 }
