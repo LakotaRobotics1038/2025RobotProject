7f511a92745aac5538a9d07ef75acc348719e496
Murat Bayraktar <bmuratb502@gmail.com>
2025-02-11 19:35:45 -0500

    Add vision

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -1,5 +1,15 @@
 package frc.robot.constants;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+
 public final class VisionConstants {
     public static final String kTableName = "Vision";
     public static final String kValuesTopic = "values";
@@ -20,4 +30,10 @@ public final class VisionConstants {
     public static final double spinD = 0.0002;
     public static final double spinSetpoint = 0.0;
     public static final double aprilTagArea = 28908;
+
+    public static final AprilTagFieldLayout kTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+    public static final Transform3d kRobotToCam = new Transform3d(new Translation3d(0.1, 0.0, 0.1),
+            new Rotation3d(0, 0, 0));
+    public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4, 4, 8);
+    public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);
 }
