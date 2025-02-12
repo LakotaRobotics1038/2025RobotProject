1f7ad3a8e6ae658d0416e7ab5eb96686cf0ec070
Griffin <lamb.griffin@icloud.com>
2025-02-12 11:13:34 -0500

    made trajectories for the paths

--- src/main/java/frc/robot/autons/Trajectories.java
+++ src/main/java/frc/robot/autons/Trajectories.java
@@ -9,8 +9,8 @@ import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.trajectory.PathPlannerTrajectory;
 import com.pathplanner.lib.util.FileVersionException;
 
-import frc.robot.subsystems.DriveTrain;
 import frc.robot.constants.AutoConstants;
+import frc.robot.subsystems.DriveTrain;
 
 public class Trajectories {
     private static PathPlannerTrajectory getTrajectory(String pathName)
@@ -23,7 +23,28 @@ public class Trajectories {
 
     }
 
-    public static PathPlannerTrajectory getSomeTrajectory() throws IOException, ParseException, FileVersionException {
+    public static PathPlannerTrajectory getMiddlePosToRightTag21Trajectory()
+            throws IOException, ParseException, FileVersionException {
         return Trajectories.getTrajectory("StartMiddlePos to RightReefTag21");
+    }
+
+    public static PathPlannerTrajectory getRightTag21ToTopFeederStationTrajectory()
+            throws IOException, ParseException, FileVersionException {
+        return Trajectories.getTrajectory("RightReefTag21 To TopFeederStation");
+    }
+
+    public static PathPlannerTrajectory getTopFeederStationToLeftTag20Trajectory()
+            throws IOException, ParseException, FileVersionException {
+        return Trajectories.getTrajectory("TopFeederStation to LeftReefTag20");
+    }
+
+    public static PathPlannerTrajectory getLeftTag20ToTopFeederStationTrajectory()
+            throws IOException, ParseException, FileVersionException {
+        return Trajectories.getTrajectory("LeftReefTag20 to TopFeederStation");
+    }
+
+    public static PathPlannerTrajectory getTopFeederStationToRightTag20Trajectory()
+            throws IOException, ParseException, FileVersionException {
+        return Trajectories.getTrajectory("TopFeederStation to RightReefTag20");
     }
 }
