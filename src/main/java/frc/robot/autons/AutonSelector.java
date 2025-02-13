0212d8c75492df67714c25994d512d9da284f5ed
Wesley Reed <24629474+reediculous456@users.noreply.github.com>
2025-02-13 16:47:26 -0500

    Discard changes to src/main/java/frc/robot/autons/AutonSelector.java

--- src/main/java/frc/robot/autons/AutonSelector.java
+++ src/main/java/frc/robot/autons/AutonSelector.java
@@ -32,8 +32,7 @@ public class AutonSelector {
         this.autoChooser = Dashboard.getInstance().getAutoChooser();
 
         this.autoChooser.setDefaultOption("No Auto", AutonChoices.NoAuto);
-        // this.autoChooser.addOption("Score 2 In Amp Position 1",
-        // AutonChoices.AmpAuto);
+        // this.autoChooser.addOption("Score 2 In Amp Position 1", AutonChoices.AmpAuto);
 
         this.delayChooser = Dashboard.getInstance().getDelayChooser();
 
@@ -58,7 +57,7 @@ public class AutonSelector {
         Optional<Alliance> alliance = DriverStation.getAlliance();
         switch (this.autoChooser.getSelected()) {
             // case AmpAuto:
-            // return new ScoreInAmp(alliance);
+            //     return new ScoreInAmp(alliance);
             default:
                 return null;
         }
