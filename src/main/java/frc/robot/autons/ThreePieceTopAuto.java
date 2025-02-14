a6bf839adcf32aeff451c9e3369d80755e4a43fc
Griffin <lamb.griffin@icloud.com>
2025-02-13 19:39:25 -0500

    finished threepiecetopauto

--- src/main/java/frc/robot/autons/ThreePieceTopAuto.java
+++ src/main/java/frc/robot/autons/ThreePieceTopAuto.java
@@ -1,17 +1,37 @@
 package frc.robot.autons;
 
-import java.nio.file.Paths;
+import java.io.IOException;
 import java.util.Optional;
 
+import org.json.simple.parser.ParseException;
+
+import com.pathplanner.lib.util.FileVersionException;
+
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.commands.AcquireCommand;
+import frc.robot.commands.DisposeCommand;
+import frc.robot.commands.SetAcquisitionPositionCommand;
+import frc.robot.commands.SetAcquisitionPositionCommand.AcquisitionPositionSetpoint;
 
 public class ThreePieceTopAuto extends Auton {
-    ThreePieceTopAuto(Optional<Alliance> alliance) {
+    ThreePieceTopAuto(Optional<Alliance> alliance) throws FileVersionException, IOException, ParseException {
         super(alliance);
 
         super.addCommands(
-        // followPathCommand()
-        );
+                followPathCommand(Paths.getMiddlePosToRightTag21Path())
+                        .alongWith(new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.L4Coral)),
+                new DisposeCommand(),
+                followPathCommand(Paths.getRightTag21ToTopFeederStationPath())
+                        .alongWith(new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.FeederStation)),
+                new AcquireCommand(),
+                followPathCommand(Paths.getTopFeederStationToLeftTag20Path())
+                        .alongWith(new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.L4Coral)),
+                new DisposeCommand(),
+                followPathCommand(Paths.getLeftTag20ToTopFeederStationPath())
+                        .alongWith(new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.FeederStation)),
+                new AcquireCommand(),
+                followPathCommand(Paths.getTopFeederStationToRightTag20Path())
+                        .alongWith(new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.L4Coral)),
+                new DisposeCommand());
     }
 }
