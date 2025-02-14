35945ebbd8f975cf674f2b74bdba4be152ac21f4
Griffin <lamb.griffin@icloud.com>
2025-02-14 09:37:11 -0500

    added a wait time before we move arm to feeder station mode

--- src/main/java/frc/robot/autons/ThreePieceTopAuto.java
+++ src/main/java/frc/robot/autons/ThreePieceTopAuto.java
@@ -8,6 +8,8 @@ import org.json.simple.parser.ParseException;
 import com.pathplanner.lib.util.FileVersionException;
 
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.AcquireCommand;
 import frc.robot.commands.DisposeCommand;
 import frc.robot.commands.SetAcquisitionPositionCommand;
@@ -22,13 +24,17 @@ public class ThreePieceTopAuto extends Auton {
                         .alongWith(new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.L4Coral)),
                 new DisposeCommand(),
                 followPathCommand(Paths.getRightTag21ToTopFeederStationPath())
-                        .alongWith(new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.FeederStation)),
+                        .alongWith(new SequentialCommandGroup(
+                                new WaitCommand(1),
+                                new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.FeederStation))),
                 new AcquireCommand(),
                 followPathCommand(Paths.getTopFeederStationToLeftTag20Path())
                         .alongWith(new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.L4Coral)),
                 new DisposeCommand(),
                 followPathCommand(Paths.getLeftTag20ToTopFeederStationPath())
-                        .alongWith(new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.FeederStation)),
+                        .alongWith(new SequentialCommandGroup(
+                                new WaitCommand(1),
+                                new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.FeederStation))),
                 new AcquireCommand(),
                 followPathCommand(Paths.getTopFeederStationToRightTag20Path())
                         .alongWith(new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.L4Coral)),
