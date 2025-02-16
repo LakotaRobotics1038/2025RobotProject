ca7022e7a42e8b314fc070b841f7ef83321e514d
Griffin <lamb.griffin@icloud.com>
2025-02-15 20:16:06 -0500

    updated current ids

--- src/main/java/frc/robot/constants/AcquisitionConstants.java
+++ src/main/java/frc/robot/constants/AcquisitionConstants.java
@@ -2,7 +2,7 @@ package frc.robot.constants;
 
 public class AcquisitionConstants {
     public static final int kTopLaserPort = 0;
-    public static final int kBottomLaserPort = 10;
+    public static final int kBottomLaserPort = 1;
     public static final int kAcquisitionMotorPort = 9;
     public static final double kAcquireSpeed = 1.0;
     public static final double kDisposeSpeed = -1.0;
--- src/main/java/frc/robot/constants/ClimbConstants.java
+++ src/main/java/frc/robot/constants/ClimbConstants.java
@@ -1,7 +1,7 @@
 package frc.robot.constants;
 
 public class ClimbConstants {
-    public static final int kClimbMotorPort = 0;
+    public static final int kClimbMotorPort = 7;
     public static final double kClimbUpSpeed = 0.0;
     public static final double kClimbDownSpeed = 0.0;
 }
