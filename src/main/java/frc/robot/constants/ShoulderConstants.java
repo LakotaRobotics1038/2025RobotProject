e9c85edca9794b20219b4c616ed367996dbe47ea
griffin <lamb.griffin@icloud.com>
2025-03-04 20:55:29 -0500

    increase shoulder power

--- src/main/java/frc/robot/constants/ShoulderConstants.java
+++ src/main/java/frc/robot/constants/ShoulderConstants.java
@@ -29,7 +29,7 @@ public class ShoulderConstants {
     public static final double kI = 0.0;
     public static final double kD = 0.0;
 
-    public static final double kMaxPower = 0.3;
+    public static final double kMaxPower = 0.75;
     public static final double kMaxDistance = 76.0;
 
     public static final double kTolerance = 0.0;
