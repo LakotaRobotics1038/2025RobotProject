a0d1bc032ccf9cd616bf375ff1a541e67ddc004d
griffin <lamb.griffin@icloud.com>
2025-03-10 18:54:36 -0400

    fixed extension conversion factor

--- src/main/java/frc/robot/constants/ExtensionConstants.java
+++ src/main/java/frc/robot/constants/ExtensionConstants.java
@@ -1,17 +1,22 @@
 package frc.robot.constants;
 
+import edu.wpi.first.units.DistanceUnit;
+import edu.wpi.first.units.Units;
+
 public class ExtensionConstants {
     public static final int kExtensionMotorPort = 2;
     public static final int kExtensionLaserPort = 36;
 
     public static final double kMinExtensionPower = -1.0;
     public static final double kMaxExtensionPower = 1.0;
-    // public static final double kExtensionMaximum = 26.14791;
-    public static final double kExtensionMaximum = 27.5;
+    public static final double kExtensionMaximum = 26.14791;
     public static final double kExtensionMaxMove = 0.0;
 
     public static final double kTolerance = 0.5;
-    public static final double kEncoderConversion = kExtensionMaximum / 122.7682;
+    private static final double kLeadScrewPitch = 80.0; // millimeters
+    private static final double kLeadScrewPitchInInches = Units.Inches.convertFrom(kLeadScrewPitch, Units.Millimeters);
+    private static final double kGearboxReduction = 16.0;
+    public static final double kEncoderConversion = kLeadScrewPitchInInches / kGearboxReduction;
     public static final double kP = 0.8;
     public static final double kI = 0.001;
     public static final double kD = 0.0;
@@ -20,14 +25,14 @@ public class ExtensionConstants {
         L1Coral(0.0),
         L2Coral(0.0),
         L3Coral(2.0),
-        L4Coral(27.5),
+        L4Coral(kExtensionMaxMove),
         L23Algae(0.0),
         L34Algae(15.0),
         Processor(0.0),
         GroundAlgae(0.0),
         Storage(0.0),
         FeederStation(3.0),
-        Barge(27.5),
+        Barge(kExtensionMaxMove),
         Climb(0.0),
         Zero(0.0);
 
