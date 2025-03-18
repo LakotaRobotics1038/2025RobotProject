1e07c8cee21a328ffdbfd080f7fee404c98e7970
Griffin <lamb.griffin@icloud.com>
2025-03-17 20:59:55 -0400

    made new algae autos

---
+++ src/main/java/frc/robot/autons/BargeProcessorAuto.java
@@ -0,0 +1,41 @@
+package frc.robot.autons;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.json.simple.parser.ParseException;
+
+import com.pathplanner.lib.util.FileVersionException;
+
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.AcquireAlgaeCommand;
+import frc.robot.commands.DisposeAlgaeCommand;
+import frc.robot.commands.SetAcquisitionPositionCommand;
+import frc.robot.commands.SetAcquisitionPositionCommand.FinishActions;
+import frc.robot.utils.AcquisitionPositionSetpoint;;
+
+public class BargeProcessorAuto extends Auton {
+    BargeProcessorAuto(Optional<Alliance> alliance) throws FileVersionException, IOException, ParseException {
+        super(alliance);
+
+        super.addCommands(
+                new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.L34Algae, FinishActions.NoDisable)
+                        .andThen(followPathCommand(Paths.getTopPathToTag20Algae())),
+                new AcquireAlgaeCommand().withDeadline(followPathCommand(Paths.getReefTag21ToNet()))
+                        .alongWith(new WaitCommand(0.5).andThen(
+                                new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.Barge,
+                                        FinishActions.NoDisable)))
+                        .withTimeout(3),
+
+                new DisposeAlgaeCommand().withTimeout(0.5),
+                new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.L23Algae, FinishActions.NoDisable),
+
+                followPathCommand(Paths.getNetToReefTag21Algae()),
+                new AcquireAlgaeCommand().withDeadline(
+                        followPathCommand(Paths.getLeftReefTag21ToProcessorPath())
+                                .alongWith(new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.Processor,
+                                        FinishActions.NoDisable))),
+                new DisposeAlgaeCommand());
+    }
+}
--- src/main/java/frc/robot/autons/ProcessorAuto.java
+++ src/main/java/frc/robot/autons/ProcessorAuto.java
@@ -10,9 +10,7 @@ import com.pathplanner.lib.util.FileVersionException;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.AcquireAlgaeCommand;
-import frc.robot.commands.AcquireForL4Command;
 import frc.robot.commands.DisposeAlgaeCommand;
-import frc.robot.commands.DisposeCoral134Command;
 import frc.robot.commands.SetAcquisitionPositionCommand;
 import frc.robot.commands.SetAcquisitionPositionCommand.FinishActions;
 import frc.robot.utils.AcquisitionPositionSetpoint;;
@@ -23,23 +21,22 @@ public class ProcessorAuto extends Auton {
 
         super.addCommands(
                 new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.L23Algae, FinishActions.NoDisable)
-                        .andThen(followPathCommand(Paths.getTopPathToTag20Algae())),
-                new AcquireAlgaeCommand().withDeadline(followPathCommand(Paths.getReefTag21ToNet())),
-
-                followPathCommand(Paths.getReefTag20ToNet())
-                        .alongWith(new WaitCommand(0.5).andThen(
-                                new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.Barge,
-                                        FinishActions.NoDisable)))
-                        .withTimeout(3),
-
+                        .andThen(followPathCommand(Paths.getMidPoseToTag21Algae())),
+                new AcquireAlgaeCommand().withDeadline(followPathCommand(Paths.getLeftReefTag21ToProcessorPath()))
+                        .alongWith(new WaitCommand(0.5)
+                                .andThen(new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.Processor,
+                                        FinishActions.NoFinish))),
                 new DisposeAlgaeCommand().withTimeout(0.5),
-                new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.Storage, FinishActions.NoDisable),
+
+                followPathCommand(Paths.getProcessorToAlgaePath22())
+                        .alongWith(
+                                new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.L34Algae,
+                                        FinishActions.NoDisable)),
 
                 new AcquireAlgaeCommand().withDeadline(
-                        followPathCommand(Paths.getNetToReefTag21Algae())).withTimeout(1),
-                new AcquireAlgaeCommand().withDeadline(
-                        followPathCommand(Paths.getLeftReefTag21ToProcessorPath())
-                                .alongWith(new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.Processor,
+                        followPathCommand(Paths.getReefTag22ToProcessor()))
+                        .alongWith(new WaitCommand(0.5)
+                                .andThen(new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.Processor,
                                         FinishActions.NoDisable))),
                 new DisposeAlgaeCommand());
     }
