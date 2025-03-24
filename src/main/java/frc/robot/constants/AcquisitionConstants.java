997b71dcd989d5910c59d71fd80c4e0fc8ca863b
Murat65536 <bmuratb502@gmail.com>
2025-03-24 18:10:49 -0400

    Optimize more to store only a fourth of the sine wave, saving even move ram.

--- src/main/java/frc/robot/constants/AcquisitionConstants.java
+++ src/main/java/frc/robot/constants/AcquisitionConstants.java
@@ -3,55 +3,55 @@ package frc.robot.constants;
 public class AcquisitionConstants {
     public static final short[] kSinLookupTable = {
             0,
+            314,
             628,
+            941,
             1253,
+            1564,
             1874,
+            2181,
             2487,
+            2790,
             3090,
+            3387,
             3681,
+            3971,
             4258,
+            4540,
             4818,
+            5090,
             5358,
+            5621,
             5878,
+            6129,
             6374,
+            6613,
             6845,
+            7071,
             7290,
+            7501,
             7705,
+            7902,
             8090,
+            8271,
             8443,
+            8607,
             8763,
+            8910,
             9048,
+            9178,
             9298,
+            9409,
             9511,
+            9603,
             9686,
+            9759,
             9823,
+            9877,
             9921,
+            9956,
             9980,
-            10000,
-            9980,
-            9921,
-            9823,
-            9686,
-            9511,
-            9298,
-            9048,
-            8763,
-            8443,
-            8090,
-            7705,
-            7290,
-            6845,
-            6374,
-            5878,
-            5358,
-            4818,
-            4258,
-            3681,
-            3090,
-            2487,
-            1874,
-            1253,
-            628
+            9995
     };
 
     public static final int kTopLaserPort = 1;
@@ -67,8 +67,13 @@ public class AcquisitionConstants {
 
     public static float getSine(double degrees) {
         double deg = degrees % (Math.PI * 2);
-        short sine = kSinLookupTable[(int) Math.round(Math.toRadians(deg) / (2 * Math.PI) * kSinLookupTable.length)
-                - 1];
+        short sine;
+        if ((deg > Math.PI * 0.5 && deg < Math.PI) || deg > Math.PI * 1.5) {
+            sine = kSinLookupTable[(int) Math.round((1 - Math.toRadians(deg) / (2 * Math.PI)) * kSinLookupTable.length)
+                    - 1];
+        } else {
+            sine = kSinLookupTable[(int) Math.round(Math.toRadians(deg) / (2 * Math.PI) * kSinLookupTable.length) - 1];
+        }
         return deg > Math.PI ? sine / 10000 : -sine / 10000;
     }
 
