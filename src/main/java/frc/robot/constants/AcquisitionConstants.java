ab063e1246a78751740a6e223026641b683bcd65
1038Pit <1038Pit@1038.org>
2025-03-27 17:56:33 -0400

    Dispose speed reduced to -0.5

--- src/main/java/frc/robot/constants/AcquisitionConstants.java
+++ src/main/java/frc/robot/constants/AcquisitionConstants.java
@@ -3,6 +3,6 @@ package frc.robot.constants;
 public class AcquisitionConstants {
     public static final int kAcquisitionMotorPort = 4;
     public static final double kAcquireAlgaeSpeed = 1.0;
-    public static final double kDisposeAlgaeSpeed = -0.75;
+    public static final double kDisposeAlgaeSpeed = -0.5;
     public static final double kShootAlgaeSpeed = -1.0;
 }
