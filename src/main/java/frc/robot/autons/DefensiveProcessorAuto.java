0d7204cd998f19d095f85fbaf9457b947a660ff9
1038Pit <1038Pit@1038.org>
2025-03-29 08:45:54 -0400

    Created Single Processor into StartingPos Auto

---
+++ src/main/java/frc/robot/autons/DefensiveProcessorAuto.java
@@ -0,0 +1,38 @@
+package frc.robot.autons;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.json.simple.parser.ParseException;
+
+import com.pathplanner.lib.util.FileVersionException;
+
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import frc.robot.commands.AcquireAlgaeCommand;
+import frc.robot.commands.DisposeAlgaeCommand;
+import frc.robot.commands.SetAcquisitionPositionCommand;
+import frc.robot.commands.SetAcquisitionPositionCommand.FinishActions;
+import frc.robot.commands.SetAcquisitionPositionEscape;
+import frc.robot.utils.AcquisitionPositionSetpoint;
+
+public class DefensiveProcessorAuto extends Auton {
+    DefensiveProcessorAuto(Optional<Alliance> alliance) throws FileVersionException, IOException, ParseException {
+        super(alliance);
+
+        super.addCommands(
+                new SetAcquisitionPositionEscape(SetAcquisitionPositionEscape.FinishActions.Default),
+                (new AcquireAlgaeCommand().withTimeout(2)).raceWith(
+                        new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.L23Algae, FinishActions.NoDisable)
+                                .alongWith(followPathCommand(Paths.getMidPoseToTag21Algae())).withTimeout(2)),
+                (new AcquireAlgaeCommand().withTimeout(2))
+                        .raceWith(followPathCommand(Paths.getReefTag21ToProcessor()))
+                        .alongWith(new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.Processor,
+                                FinishActions.NoDisable).withTimeout(2)),
+                new DisposeAlgaeCommand().withTimeout(0.5),
+
+                new SetAcquisitionPositionCommand(AcquisitionPositionSetpoint.Defense,
+                        SetAcquisitionPositionCommand.FinishActions.NoFinish)
+                        .raceWith(followPathCommand(Paths.getProcessorTaxi())));
+    }
+
+}
