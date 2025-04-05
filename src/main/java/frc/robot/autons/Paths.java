53928c8fba7d87a72b7a5223d3878eb47809f25e
griffin <lamb.griffin@icloud.com>
2025-04-05 13:04:44 -0400

    deleted procsesor auto

--- src/main/java/frc/robot/autons/Paths.java
+++ src/main/java/frc/robot/autons/Paths.java
@@ -18,39 +18,9 @@ public class Paths {
         return PathPlannerPath.fromPathFile("BottomStartingPos Taxi");
     }
 
-    public static PathPlannerPath getLeftReefTag21ForwardsPath()
-            throws IOException, ParseException, FileVersionException {
-        return PathPlannerPath.fromPathFile("LeftReefTag21 Forwards");
-    }
-
-    public static PathPlannerPath getLeftReefTag21BackwardsPath()
-            throws IOException, ParseException, FileVersionException {
-        return PathPlannerPath.fromPathFile("LeftReefTag21 Backwards");
-    }
-
-    public static PathPlannerPath getReefTag21ToProcessor()
-            throws IOException, ParseException, FileVersionException {
-        return PathPlannerPath.fromPathFile("Reef 21 to processor");
-    }
-
-    public static PathPlannerPath getReefTag22ToProcessor()
-            throws IOException, ParseException, FileVersionException {
-        return PathPlannerPath.fromPathFile("Reef Tag 22 Algae To Processor");
-    }
-
-    public static PathPlannerPath getProcessorToAlgaePath22()
-            throws IOException, ParseException, FileVersionException {
-        return PathPlannerPath.fromPathFile("Processor to Algae Tag 22");
-    }
-
     public static PathPlannerPath getMidPoseToTag21Algae()
             throws IOException, ParseException, FileVersionException {
         return PathPlannerPath.fromPathFile("Mid Pose to 21 Algae");
-    }
-
-    public static PathPlannerPath getNetToTag20Algae()
-            throws IOException, ParseException, FileVersionException {
-        return PathPlannerPath.fromPathFile("Net to Tag 20 Algae");
     }
 
     public static PathPlannerPath getReefTag21ToNet()
@@ -58,18 +28,13 @@ public class Paths {
         return PathPlannerPath.fromPathFile("Reef Tag 21 To Net");
     }
 
-    public static PathPlannerPath getNetToReefTag22Algae()
+    public static PathPlannerPath getNetToTag20Algae()
             throws IOException, ParseException, FileVersionException {
-        return PathPlannerPath.fromPathFile("Net to Reef Tag 22 Algae");
+        return PathPlannerPath.fromPathFile("Net to Tag 20 Algae");
     }
 
     public static PathPlannerPath getReefTag20ToNet()
             throws IOException, ParseException, FileVersionException {
         return PathPlannerPath.fromPathFile("Reef Tag 20 To Net");
-    }
-
-    public static PathPlannerPath getProcessorTaxi()
-            throws IOException, ParseException, FileVersionException {
-        return PathPlannerPath.fromPathFile("Processor Taxi");
     }
 }
